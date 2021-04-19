bf312a09e3d1f2d85f51ee00df5f1cde61f0ecad
Norton <kevin197011@outlook.com>
2021-04-19 12:47:04 +0800

    Update.

--- vars/hello.groovy
+++ vars/hello.groovy
@@ -10,12 +10,11 @@ def call() {
         agent any
 
 
-    parameters {[
+    parameters {
 
-        string(defaultValue: 'user1', name: 'username', trim: true, description: 'username'),
-        booleanParam(defaultValue: false, name: 'isOk', description: 'sure?'),
-        choice(choices: ['A', 'B', 'C'], name: 'item', description: 'which one?'),
-    ]
+        string(defaultValue: 'user1', name: 'username', trim: true, description: 'username')
+        booleanParam(defaultValue: false, name: 'isOk', description: 'sure?')
+        choice(choices: ['A', 'B', 'C'], name: 'item', description: 'which one?')
     }
 
 
