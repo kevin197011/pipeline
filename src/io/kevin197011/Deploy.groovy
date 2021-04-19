c7c224a1fec3d42ce49e6a934b1875b557c8dd39
Norton <kevin197011@outlook.com>
2021-04-19 15:41:34 +0800

    Update.

---
+++ src/io/kevin197011/Deploy.groovy
@@ -0,0 +1,19 @@
+package io.kevin197011
+
+class Deploy {
+    private String gitRepo
+    private String hostIp
+
+    Deploy(String gitRepo, String hostIp) {
+        this.gitRepo = gitRepo
+        this.hostIp = hostIp
+    }
+
+    @Override
+    public String toString() {
+        return "Deploy{" +
+                "gitRepo='" + gitRepo + '\'' +
+                ", hostIp='" + hostIp + '\'' +
+                '}';
+    }
+}
---
+++ vars/deploy.groovy
@@ -0,0 +1,79 @@
+#!/usr/bin/env groovy
+
+import io.kevin197011.Deploy
+
+
+def call() {
+    //load sharelibrary
+    // def hello = new hello()
+    // def time = new time()
+    Deploy deploy = new Deploy("https://github.com/test", "1.2.3.4")
+
+    //pipeline
+    pipeline {
+        // agent { node { label "build"}}
+        agent any
+
+        parameters {
+            string(name: 'username', defaultValue: 'user1', trim: true, description: 'username')
+            booleanParam(name: 'Ok', defaultValue: true, description: 'ok?')
+            choice(name: 'item', choices: ['A', 'B', 'C'], description: 'which one?')
+        }
+
+        stages {
+            stage('test') {
+                steps {
+                    script {
+                        println(deploy)
+                    }
+                }
+            }
+
+            stage('parallel') {
+                parallel {
+                    stage('A') {
+                        steps {
+                            script {
+                                printf('A %s', Time.timeFormat())
+                                sleep(10)
+                            }
+                        }
+                    }
+                    stage('B') {
+                        steps {
+                            script {
+                                printf('B %s', Time.timeFormat())
+                                sleep(10)
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        post {
+            always {
+                script {
+                    println('always')
+                }
+            }
+
+            success {
+                script {
+                    println('success')
+                }
+            }
+            failure {
+                script {
+                    println('failure')
+                }
+            }
+
+            aborted {
+                script {
+                    println('aborted')
+                }
+            }
+        }
+    }
+}
