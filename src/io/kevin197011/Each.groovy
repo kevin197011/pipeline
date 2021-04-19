42f7bc5d5775bed6e12f56ad7630d04c4c092330
Norton <kevin197011@outlook.com>
2021-04-19 15:27:24 +0800

    Update.

--- src/io/kevin197011/Each.groovy
+++ src/io/kevin197011/Each.groovy
@@ -1,9 +1,10 @@
 package io.kevin197011
 
+/**
+ * each class
+ */
 class Each {
-
     static String printMsg(String name) {
         return sprintf("hello %s", name)
     }
-
 }
--- src/io/kevin197011/Time.groovy
+++ src/io/kevin197011/Time.groovy
@@ -2,6 +2,9 @@ package io.kevin197011
 
 import java.text.SimpleDateFormat
 
+/**
+ * Time class
+ */
 class Time {
     static String timeFormat() {
         return new SimpleDateFormat('MM/dd/yyyy HH:mm:ss').format(new Date()).toString()
