2f9207e6ed1d221ac7bb65c8cd5b1bd61277c156
Norton <kevin197011@outlook.com>
2021-04-20 09:14:57 +0800

    Update.

--- src/io/kevin197011/Git.groovy
+++ src/io/kevin197011/Git.groovy
@@ -2,11 +2,11 @@ package io.kevin197011
 
 class Git {
 
-    static void gitClone(String gitRepo, String path){
+    static void gitClone(String gitRepo, String path) {
         // TODO
     }
 
-    static void gitPull(String gitRepo, String path){
+    static void gitPull(String gitRepo, String path) {
         // TODO
     }
 }
