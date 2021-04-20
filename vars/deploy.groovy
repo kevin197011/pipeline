1e1a569c70cf57620a5e35a3426082aff40ff0c8
Norton <kevin197011@outlook.com>
2021-04-20 08:11:48 +0800

    Update.

--- vars/deploy.groovy
+++ vars/deploy.groovy
@@ -5,87 +5,88 @@ import io.kevin197011.Deploy
 def call(body) {
 
     def config = [
-            name: "devops",
-            age : "18",
-            id  : "123456"
+            'name': "devops",
+            'age' : "18",
+            'id'  : "123456"
     ]
     body.resolveStrategy = Closure.DELEGATE_FIRST
     body.delegate = config
     body()
 
     def gitRepo = "https://github.com/test"
-    def hostIp = "https://github.com/test"
+    def hostIp = "xxx.xxx.xxx.xxx"
 
     def deploy = new Deploy(gitRepo, hostIp)
 
+    config.
 
-    //pipeline
-    pipeline {
-        // agent { node { label "build"}}
-        agent any
+            //pipeline
+                    pipeline {
+                // agent { node { label "build"}}
+                agent any
 
-        parameters {
-            string(name: 'username', defaultValue: 'user1', trim: true, description: 'username')
-            booleanParam(name: 'Ok', defaultValue: true, description: 'ok?')
-            choice(name: 'item', choices: ['A', 'B', 'C'], description: 'which one?')
-        }
-
-        stages {
-            stage('test') {
-                steps {
-                    script {
-                        println(deploy.toString())
-                        config.each {
-                            printf("%s => %s\n", it.key, it.value)
-                        }
-                    }
+                parameters {
+                    string(name: 'username', defaultValue: 'user1', trim: true, description: 'username')
+                    booleanParam(name: 'Ok', defaultValue: true, description: 'ok?')
+                    choice(name: 'item', choices: ['A', 'B', 'C'], description: 'which one?')
                 }
-            }
 
-            stage('parallel') {
-                parallel {
-                    stage('A') {
+                stages {
+                    stage('test') {
                         steps {
                             script {
-                                sleep(10)
+                                println(deploy.toString())
+                                config.each { key, val ->
+                                    printf("%s => %s\n", $key, $value)
+                                }
                             }
                         }
                     }
-                    stage('B') {
-                        steps {
-                            script {
-                                sleep(10)
+
+                    stage('parallel') {
+                        parallel {
+                            stage('A') {
+                                steps {
+                                    script {
+                                        sleep(10)
+                                    }
+                                }
+                            }
+                            stage('B') {
+                                steps {
+                                    script {
+                                        sleep(10)
+                                    }
+                                }
                             }
                         }
                     }
                 }
-            }
-        }
 
-        post {
-            always {
-                script {
-                    println('always')
+                post {
+                    always {
+                        script {
+                            println('always')
+                        }
+                    }
+
+                    success {
+                        script {
+                            println('success')
+                        }
+                    }
+
+                    failure {
+                        script {
+                            println('failure')
+                        }
+                    }
+
+                    aborted {
+                        script {
+                            println('aborted')
+                        }
+                    }
                 }
             }
-
-            success {
-                script {
-                    println('success')
-                }
-            }
-
-            failure {
-                script {
-                    println('failure')
-                }
-            }
-
-            aborted {
-                script {
-                    println('aborted')
-                }
-            }
-        }
-    }
 }
