99f9d0ff7a88e94c2236ee51cb6462ebfb817fa6
kk <61411931+kevin197011@users.noreply.github.com>
2023-07-05 16:07:48 +0800

    Update app.groovy

--- vars/app.groovy
+++ vars/app.groovy
@@ -9,11 +9,16 @@ def call() {
         // agent { node { label "Build"}}
         agent any
 
+        environment {
+            SERVER_KEY = credentials('349f9a1e-b4a0-4f1a-98cf-b0574ccf1b54')   
+        }
+        
         stages {
             stage('test') {
                 steps {
                     script {
                         println data
+                        println $SERVER_KEY_USR
                     }
                 }
             }
