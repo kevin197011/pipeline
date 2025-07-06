ad7e451ed16a8f84745bc46242bc2b54122e277e
Norton <kevin197011@outlook.com>
2025-07-06 08:04:42 +0800

    Update 2025-07-06 08:04:38 +0800

--- src/io/kevin197011/cicd/AbstractDeploy.groovy
+++ src/io/kevin197011/cicd/AbstractDeploy.groovy
@@ -2,18 +2,18 @@ package io.kevin197011.cicd
 
 /**
  * class AbstractDeploy
+ * 适用于 pipeline，所有核心方法带 steps 参数
  */
-
 abstract class AbstractDeploy implements Gitable, Deployable {
 
     @Override
-    abstract boolean gitClone(String repo)
+    abstract boolean gitClone(String repo, def steps)
 
     @Override
-    abstract boolean gitPull(String path)
+    abstract boolean gitPull(String path, def steps)
 
     @Override
-    abstract boolean doDeploy()
+    abstract boolean doDeploy(def steps)
 
     @Override
     abstract void validate()
